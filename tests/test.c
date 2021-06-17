c9c71b539ca8814e42cc12334fa4239533a51b9e
Ryan Lee <ryanjlee22@gmail.com>
2021-06-17 19:42:05 -0400

    Update test file

--- tests/test.c
+++ tests/test.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 #include "../src/sequence_repetition.h"
+#include "../src/sequence_repetition.h"
 
-//Compile code: gcc test.c ../src/sequence_repetition.c
+//Compile code: gcc test.c ../src/sequence_repetition.c ../src/integer_array.c
+
 
 int main() {
 
-	int index = findDNARepetition("AAABBABBABA", "BBAB");
+
+	int index = findDNARepetition("ABAABABBABABA", "BBAB");
 
 	printf("Index of Repetition: %d\n", index);
 
+	struct IntegerArray* indices = findAllDNARepetitions("BABABABA", "BA");
 
+	struct Node* current = indices->root;
+
+	for (int i = 0; i < indices->count; i++) {
+		printf("Index: %d\n", current->value);
+		current = current->next_node;
+	}
 }
