b43d25a34ff99675cb05ad3f19581fe63fdcf4e1
Ryan Lee <ryanjlee22@gmail.com>
2021-06-17 19:40:39 -0400

    Add method for locating all repetitions

--- src/sequence_repetition.c
+++ src/sequence_repetition.c
@@ -27,11 +27,8 @@ int findDNARepetition(const char* sequence, const char* motif) {
 		leftBaseOffset = (leftBaseOffset * 256) % PRIME_MODULUS;
 	}
 
-	printf("Left base offset: %d\n", leftBaseOffset);
-
 	//Computing rolling hash
-	for (int i = 0; i < strlen(sequence) - strlen(motif); i++) {
-		printf("R and H: %d %d\n", r, h);
+	for (int i = 0; i < strlen(sequence) - strlen(motif) + 1; i++) {
 
 		if (r == h) {
 			strncpy(possibleSub, sequence + i, strlen(motif));
@@ -52,3 +49,46 @@ int findDNARepetition(const char* sequence, const char* motif) {
 	return -1;
 
 }
+
+struct IntegerArray* findAllDNARepetitions(const char* sequence, const char* motif) {
+	int r = getSequenceHash(motif);
+
+	//First characters of the sequence
+	char possibleSub[strlen(motif) + 1];
+	strncpy(possibleSub, sequence, strlen(motif));
+	possibleSub[strlen(motif)] = '\0';
+
+	int h = getSequenceHash(possibleSub);
+
+	int leftBaseOffset = 1;
+
+	for (int i = 0; i < strlen(motif) - 1; i++) {
+		leftBaseOffset = (leftBaseOffset * 256) % PRIME_MODULUS;
+	}
+
+	struct IntegerArray* integerArray = malloc(sizeof(struct IntegerArray));
+	integerArray->count = 0;
+
+	//Computing rolling hash
+	for (int i = 0; i < strlen(sequence) - strlen(motif) + 1; i++) {
+
+		if (r == h) {
+			strncpy(possibleSub, sequence + i, strlen(motif));
+			if (strcmp(motif, possibleSub) == 0) {
+				addItem(integerArray, i);
+			}
+		}
+		h += PRIME_MODULUS - ((leftBaseOffset * sequence[i]) % PRIME_MODULUS); 
+		h *= 256;
+		h += sequence[i + strlen(motif)];
+		h = h % PRIME_MODULUS;
+
+		if (h < 0) {
+			h += PRIME_MODULUS;
+		}
+	}
+
+	return integerArray;
+
+}
+
--- src/sequence_repetition.h
+++ src/sequence_repetition.h
@@ -5,11 +5,16 @@
 #include <string.h>
 #include <stdio.h>
 
+#include "integer_array.h"
+
 
 #define PRIME_MODULUS 101
 
 //Returns the index of the first repetition
-int findDNARepetition(const char* sequence, const char* subsequence);
+int findDNARepetition(const char* sequence, const char* motif);
+
+//Returns indices of all repetitions
+struct IntegerArray* findAllDNARepetitions(const char* sequence, const char* motif);
 
 
 #endif
